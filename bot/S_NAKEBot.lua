6207a4834e0b768da698a6b85c43ad0679921b97
alihbs <alihbsgr@yahoo.com>
2015-12-22 16:18:47 -0800

    Update S_NAKEBot.lua

--- bot/S_NAKEBot.lua
+++ bot/S_NAKEBot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {,178618864,90830884,0,tonumber(our_id)},--Sudo users
+    sudo_users = {178618864,90830884,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
